ea1eb3bfb061b14f14a5de52cee86941b03c1f82
auzigog <auzigog@gmail.com>
2009-01-10 19:47:20 -0800

    added simple mediawiki code

---
+++ mediawiki_api_wrapper.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of mediawiki_api_wrapper
+ *
+ * @author eyeRmonkey
+ */
+class mediawiki_api_wrapper {
+    //put your code here
+}
+?>
